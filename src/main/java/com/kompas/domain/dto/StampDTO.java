b5cb3bfead4d9cd3929d203ac3549aebc9a918d3
liravanigroup <liravanigroup@gmail.com>
2017-02-08 21:56:59 +0100

    Refactor StampDTO

--- src/main/java/com/kompas/domain/dto/StampDTO.java
+++ src/main/java/com/kompas/domain/dto/StampDTO.java
@@ -1,51 +1,43 @@
 package com.kompas.domain.dto;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-/**
- * Created by Sergej Povzanyuk on 08.08.2016.
- */
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
 public class StampDTO {
+    private static final String EMPTY_STRING_VALUE = "";
 
-    private static final StringProperty EMPTY_STRING_VALUE = new SimpleStringProperty("");
-
-    private StringProperty ksStPartNumber = EMPTY_STRING_VALUE;
-    private StringProperty ksStDescription = EMPTY_STRING_VALUE;
-    private StringProperty ksStMaterial = EMPTY_STRING_VALUE;
-    private StringProperty ksStMass = EMPTY_STRING_VALUE;
-    private StringProperty ksStScale = EMPTY_STRING_VALUE;
-    private StringProperty ksStSheetNumber = EMPTY_STRING_VALUE;
-    private StringProperty ksStNumberOfSheets = EMPTY_STRING_VALUE;
-    private StringProperty ksStCompany = EMPTY_STRING_VALUE;
-    private StringProperty ksStTypeOfWork = EMPTY_STRING_VALUE;
-    private StringProperty ksStDocumentLetter1 = EMPTY_STRING_VALUE;
-    private StringProperty ksStDocumentLetter2 = EMPTY_STRING_VALUE;
-    private StringProperty ksStDocumentLetter3 = EMPTY_STRING_VALUE;
-    private StringProperty ksStFullFileName = EMPTY_STRING_VALUE;
-    private StringProperty ksStShortFileName = EMPTY_STRING_VALUE;
-    private StringProperty ksStMarkingLine = EMPTY_STRING_VALUE;
-    private StringProperty ksStDocumentName = EMPTY_STRING_VALUE;
-    private StringProperty ksStDocumentCode = EMPTY_STRING_VALUE;
-    private StringProperty ksStOKPCode = EMPTY_STRING_VALUE;
-    private StringProperty ksStAuthor = EMPTY_STRING_VALUE;
-    private StringProperty ksStCheckedBy = EMPTY_STRING_VALUE;
-    private StringProperty ksStMfgApprovedBy = EMPTY_STRING_VALUE;
-    private StringProperty ksStDesigner = EMPTY_STRING_VALUE;
-    private StringProperty ksStRateOfInspection = EMPTY_STRING_VALUE;
-    private StringProperty ksStApprovedBy = EMPTY_STRING_VALUE;
-    private StringProperty ksStEndDesignDate = EMPTY_STRING_VALUE;
-    private StringProperty ksStCheckedDate = EMPTY_STRING_VALUE;
-    private StringProperty ksStMfgApprovedDate = EMPTY_STRING_VALUE;
-    private StringProperty ksStExecutionDate = EMPTY_STRING_VALUE;
-    private StringProperty ksStRateOfInspectionDate = EMPTY_STRING_VALUE;
-    private StringProperty ksStApprovedDate = EMPTY_STRING_VALUE;
+    private String ksStPartNumber = EMPTY_STRING_VALUE;
+    private String ksStDescription = EMPTY_STRING_VALUE;
+    private String ksStMaterial = EMPTY_STRING_VALUE;
+    private String ksStMass = EMPTY_STRING_VALUE;
+    private String ksStScale = EMPTY_STRING_VALUE;
+    private String ksStSheetNumber = EMPTY_STRING_VALUE;
+    private String ksStNumberOfSheets = EMPTY_STRING_VALUE;
+    private String ksStCompany = EMPTY_STRING_VALUE;
+    private String ksStTypeOfWork = EMPTY_STRING_VALUE;
+    private String ksStDocumentLetter1 = EMPTY_STRING_VALUE;
+    private String ksStDocumentLetter2 = EMPTY_STRING_VALUE;
+    private String ksStDocumentLetter3 = EMPTY_STRING_VALUE;
+    private String ksStFullFileName = EMPTY_STRING_VALUE;
+    private String ksStShortFileName = EMPTY_STRING_VALUE;
+    private String ksStMarkingLine = EMPTY_STRING_VALUE;
+    private String ksStDocumentName = EMPTY_STRING_VALUE;
+    private String ksStDocumentCode = EMPTY_STRING_VALUE;
+    private String ksStOKPCode = EMPTY_STRING_VALUE;
+    private String ksStAuthor = EMPTY_STRING_VALUE;
+    private String ksStCheckedBy = EMPTY_STRING_VALUE;
+    private String ksStMfgApprovedBy = EMPTY_STRING_VALUE;
+    private String ksStDesigner = EMPTY_STRING_VALUE;
+    private String ksStRateOfInspection = EMPTY_STRING_VALUE;
+    private String ksStApprovedBy = EMPTY_STRING_VALUE;
+    private String ksStEndDesignDate = EMPTY_STRING_VALUE;
+    private String ksStCheckedDate = EMPTY_STRING_VALUE;
+    private String ksStMfgApprovedDate = EMPTY_STRING_VALUE;
+    private String ksStExecutionDate = EMPTY_STRING_VALUE;
+    private String ksStRateOfInspectionDate = EMPTY_STRING_VALUE;
+    private String ksStApprovedDate = EMPTY_STRING_VALUE;
 }

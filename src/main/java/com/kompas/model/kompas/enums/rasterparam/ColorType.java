fc4024cca080b15094a4f03300ca0080235f59dc
liravanigroup <liravanigroup@gmail.com>
2017-02-12 10:44:55 +0100

    ColorTypeEnum

--- src/main/java/com/kompas/model/kompas/enums/rasterparam/ColorType.java
+++ src/main/java/com/kompas/model/kompas/enums/rasterparam/ColorType.java
@@ -4,25 +4,17 @@ package com.kompas.model.kompas.enums.rasterparam;
  * Created by Amsterdam on 29.06.2016.
  */
 
-/**
- * BLACKWHITE  0 - черный,
- * COLORVIEW 1 - цвет, установленный для вида,
- * COLORLAYER 2 - цвет, установленный для слоя,
- * COLOROBJECT 3 - цвет, установленный для объекта.
- */
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum ColorType {
-    BLACKWHITE(0),
-    COLORVIEW(1),
-    COLORLAYER(2),
-    COLOROBJECT(3);
+    BLACKWHITE(0, "черный"),
+    COLORVIEW(1, "цвет, установленный для вида"),
+    COLORLAYER(2, "цвет, установленный для слоя"),
+    COLOROBJECT(3, "цвет, установленный для объекта");
 
-    private int colorType;
-
-    ColorType(int colorType) {
-        this.colorType = colorType;
-    }
-
-    public int value() {
-        return colorType;
-    }
+    private int index;
+    private String name;
 }
